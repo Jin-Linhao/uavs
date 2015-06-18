fabba5bc246931ddf8b8b579bb0ddce48fe67ce5
eee <Wang Chen>
2015-06-18 20:44:29 +0800

    fix bug in cam test launchfile

--- src/vision/camera/src/display.cpp
+++ src/vision/camera/src/display.cpp
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
   cv::namedWindow("view");
   cv::startWindowThread();
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("/camera_raw", 1, imageCallback);
+  image_transport::Subscriber sub = it.subscribe("camera/image", 1, imageCallback);
   ros::spin();
   cv::destroyWindow("view");
 }

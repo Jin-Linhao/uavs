b13fbc8febe50f1de750b581d580b834a1ac21e9
Jeffsan <Jeffsan>
2015-09-14 13:13:24 +0800

    add find optimal parameter file

--- src/visualuwb/script/optimal_para.py
+++ src/visualuwb/script/optimal_para.py
@@ -3,5 +3,12 @@ if __name__=='__main__':
     a=[0.000001*2**i for i in xrange(0,25)]
     t=[i for i in xrange(0,25)]
     plt.plot(a,'o')
-    plt.show()
-    print a
+    #plt.show()
+    A = array([[0,0,1,0],
+               [0,0,0,1],
+               [0,0,0,0],
+               [0,0,0,0]])
+    print scipy.linalg.expm(A*0.3)
+    
+    #print time.time()-start
+    #print a
